3a4bdcc179e3779d0af5524f04ce0981664407bb
Krzysztof <76277851+KrzysztofKan@users.noreply.github.com>
2023-04-22 11:27:18 +0200

    Update dzien1.py

--- dzien1.py
+++ dzien1.py
@@ -1,18 +1,36 @@
-A = (-12, 2)
-B = (-5, 11)
-C = (5, 24)
-N = 153
-check = False
 
-print('Nalezy do :', end=' ')
-if A[0] <= N <= A[1]:
-    check = True
-    print('A', end=' ')
-if B[0] <= N <= B[1]:
-    check = True
-    print('B', end=' ')
-if C[0] <= N <= C[1]:
-    check = True
-    print('C', end='\n')
-if check == False:
-    print('zadnego zbioru')
+print('zadanie1:', end=' ')
+start_z1 = 0
+end_z1 = 100
+while start_z1 < end_z1 +1:
+    print(start_z1, end=' ')
+    start_z1 +=1
+print('\n')
+
+
+print('zadanie2:', end=' ')
+start_z2 = 50
+end_z2 = -50
+while start_z2 > end_z2 -1:
+    print(start_z2, end=' ')
+    start_z2 -=1
+print('\n')
+
+
+print('zadanie3:', end=' ')
+start_z3 = 50
+end_z3 = -50
+while start_z3 > end_z3 -1:
+    print(start_z3, end=' ')
+    start_z3 -=5
+print('\n')
+
+
+print('zadanie4:', end=' ')
+start_z4 = -30
+end_z4 = 30
+while start_z4 < end_z4 +1:
+    if start_z4 %2 == 0:
+        print(start_z4, end=' ')
+    start_z4 +=1
+print('\n')
