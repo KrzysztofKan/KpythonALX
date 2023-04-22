a622cc9e802bb178ba802eec5416801970e28702
Krzysztof <76277851+KrzysztofKan@users.noreply.github.com>
2023-04-22 11:03:21 +0200

    Update dzien1.py

--- dzien1.py
+++ dzien1.py
@@ -1,14 +1,18 @@
 A = (-12, 2)
 B = (-5, 11)
 C = (5, 24)
-N = 6
+N = 153
+check = False
 
 print('Nalezy do :', end=' ')
 if A[0] <= N <= A[1]:
+    check = True
     print('A', end=' ')
 if B[0] <= N <= B[1]:
+    check = True
     print('B', end=' ')
 if C[0] <= N <= C[1]:
+    check = True
     print('C', end='\n')
-else:
+if check == False:
     print('zadnego zbioru')
